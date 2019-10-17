7e5daae471cdbf2e3b649082037324c1238f4c42
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-10-17 13:33:16 +1100

    Assume type is iterable

--- src/OnlineStatsBase.jl
+++ src/OnlineStatsBase.jl
@@ -105,7 +105,6 @@ end
 fit!(o::OnlineStat{T}, yi::T) where {T} = (_fit!(o, yi); return o)
 
 function fit!(o::OnlineStat{I}, y::T) where {I, T}
-    Base.isiterable(T) || error("Cannot iterate through type ",T)
 
     T == eltype(T) && error("Entered recursive loop. The input type for $(name(o,false,false)) is $I. Found $(eltype(y)) in iterable type $T.")
 
