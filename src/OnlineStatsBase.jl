7f28970183945131ff08a043e81737b63d71c526
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-10-17 13:10:11 +1100

    Align test with intent stated in docblock
    Although this change has better alignment with the “docblock”, it doesn’t work when `T` is of type `Base.Generator`, whose eltype is `Any`.

--- src/OnlineStatsBase.jl
+++ src/OnlineStatsBase.jl
@@ -105,7 +105,7 @@ end
 fit!(o::OnlineStat{T}, yi::T) where {T} = (_fit!(o, yi); return o)
 
 function fit!(o::OnlineStat{I}, y::T) where {I, T}
-    T == eltype(y) && error("The input for $(name(o,false,false)) is a $I.  Found $T.")
+    eltype(y) <: I || error("The input for $(name(o,false,false)) is a $I.  Found $(eltype(y)) in iterable type $T.")
     for yi in y
         fit!(o, yi)
     end
